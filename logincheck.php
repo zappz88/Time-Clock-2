4413bf98bfb415dc89d8ae0f211e07f1f6bc5a72
zappz88 <31833538+zappz88@users.noreply.github.com>
2018-12-16 23:23:28 -0600

    Introduced new elements
    Added $_SESSION for universal ID use and modified target page for successful login.

--- logincheck.php
+++ logincheck.php
@@ -1,10 +1,13 @@
 <?php
+
+session_start();
+$_SESSION['id'] = $_POST["id"];
+$userid = $_SESSION['id'];
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "logindb";
-
-$userid = $_POST["ID"];
+$dbname = "mydb";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -13,14 +16,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create database
-$sql = "Select * from ids where ID = '$userid'";
+// Check data
+$sql = "Select * from employees where ID = '$userid'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) == 0) {
     echo "Improper Login";
 } else {
-    header("Location: http://localhost/PhpProject2/clockin.php?ID=");
+    header("Location: http://localhost/PhpProject2/clockin.php");
 }
 
 mysqli_close($conn);
