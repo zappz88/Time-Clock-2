1864fab6f4f4b39aacbcd783c25c984e03dee2b8
zappz88 <31833538+zappz88@users.noreply.github.com>
2018-12-16 23:20:14 -0600

    Introduced $_SESSION variable to store ID data
    Allows for universal implementation of user logins without code modification.

--- php-onclockout.php
+++ php-onclockout.php
@@ -1,18 +1,23 @@
 <?php
+
+session_start();
+$userid = $_SESSION['id'];
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mydb";
-$time = date('Y-m-d H:i:s');
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create database
-$sql = "INSERT INTO employees (FirstName, LastName, Status, Time) VALUES ('Andrew', 'Ramshaw', 'Clocked Out', '$time');";
+// Insert data
+$sql = "INSERT INTO times (UserID, Status) VALUES ('$userid', 'Clocked Out');";
 if (mysqli_query($conn, $sql)) {
     echo "Data added successfully";
 } else {
@@ -21,3 +26,4 @@ if (mysqli_query($conn, $sql)) {
 
 mysqli_close($conn);
 ?>
+
