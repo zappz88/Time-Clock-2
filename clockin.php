3e8e8c367125e4327ecb28cd45babc6d953642ae
zappz88 <31833538+zappz88@users.noreply.github.com>
2018-12-23 21:52:50 -0600

    Separated Logic from Presentation
    Added listeners to JS to improve functionality and readability.
    Added IDs for reference.

--- clockin.php
+++ clockin.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,27 +13,30 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
+        
         <div class="main">
 
             <h3 id="time">Current time is: </h3>
 
             <div class="btns-cont">
                 <div class="btnstxt">
-                    <p id="clockin">Clock-in time: </p>
-                    <p id="clockout">Clock-out time: </p>
+                    <p id="clockIn">Clock-in time: </p>
+                    <p id="clockOut">Clock-out time: </p>
                 </div>
+                
                 <div class="btns">       
-                    <button type="button" onclick="clockIn('clockin'); resetBtnTxt('clockin', clock.inText)">Clock In</button>
-                    <button type="button" onclick="clockOut('clockout'); resetBtnTxt('clockout', clock.outText2)">Clock Out</button>
+                    <button type="button" id="clockInBtn">Clock In</button>
+                    <button type="button" id="clockOutBtn">Clock Out</button>
                 </div>
 
                 <div  class="btnstxt">
-                    <p id="breakout">Break-out time: </p>
-                    <p id="breakin">Break-in time: </p>
+                    <p id="breakOutBtn">Break-out time: </p>
+                    <p id="breakInBtn">Break-in time: </p>
                 </div>
+                
                 <div class="btns">
-                    <button type="button" onclick="breakOut('breakout'); resetBtnTxt('breakout', rest.outText)">Break Out</button>
-                    <button type="button" onclick="breakIn('breakin'); resetBtnTxt('breakin', rest.inText2)">Break In</button>
+                    <button type="button" id="breakOutBtn">Break Out</button>
+                    <button type="button" id="breakInBtn">Break In</button>
                 </div>
             </div>
 
