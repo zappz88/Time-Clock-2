541204b9cade95e903eabd522c7624e45e84d78f
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-01-14 17:44:57 -0600

    Changed to in-page JS.
    External file issues led to program not reading JS file until after DOM creation leading to reference errors regardless of where the external call was placed. After placing JS directly on the page and at the bottom I have been having no reference errors.

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
+<?php
+session_start();
+$_SESSION['errlogin'] = "Improper Login";
+?>
+
 <html>
     <head>
         <title>Time Clock</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="timeclock.css">
-        <script src="timeclock.js"></script>
+        <link rel="stylesheet" href="timeclockphp.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
@@ -15,29 +19,92 @@
 
             <div class="btns-cont">
                 <form method="POST" action="php_checklogin.php" class="loginInfo" target="_self">
-                    ID: <input type="text" id="login" name="id" placeholder="Login ID">
+                    ID: <input type="text" id="login" name="id" value="">
                     <input type="submit" value="Login">
                 </form>
-                
+
                 <div class="loginLayout">
-                    <button type="button" id="btn0" class="btnLogin" onclick="login()">0</button>
-                    <button type="button" id="btn1" class="btnLogin" onclick="login()">1</button>
-                    <button type="button" id="btn2" class="btnLogin" onclick="login()">2</button>
-                    <button type="button" id="btn3" class="btnLogin" onclick="login()">3</button>
-                    <button type="button" id="btn4" class="btnLogin" onclick="login()">4</button>
-                    <button type="button" id="btn5" class="btnLogin" onclick="login()">5</button>
-                    <button type="button" id="btn6" class="btnLogin" onclick="login()">6</button>
-                    <button type="button" id="btn7" class="btnLogin" onclick="login()">7</button>
-                    <button type="button" id="btn8" class="btnLogin" onclick="login()">8</button>
-                    <button type="button" id="btn9" class="btnLogin" onclick="login()">9</button>
+                    <button type="button" id="btn0" class="btnLogin">0</button>
+                    <button type="button" id="btn1" class="btnLogin">1</button>
+                    <button type="button" id="btn2" class="btnLogin">2</button>
+                    <button type="button" id="btn3" class="btnLogin">3</button>
+                    <button type="button" id="btn4" class="btnLogin">4</button>
+                    <button type="button" id="btn5" class="btnLogin">5</button>
+                    <button type="button" id="btn6" class="btnLogin">6</button>
+                    <button type="button" id="btn7" class="btnLogin">7</button>
+                    <button type="button" id="btn8" class="btnLogin">8</button>
+                    <button type="button" id="btn9" class="btnLogin">9</button>
                 </div>
-                
+
                 <div id="resetBtnDiv" class="resetBtnDiv">
-                    <button type="button" id="btnReset" class="btnLoginReset" onclick="login()">Reset</button>
+                    <button type="button" id="btnReset" class="btnLoginReset">Reset</button>
                 </div>
-                
+
             </div>
 
         </div>
+        <script>
+            function login() {
+                $(function () {
+                    var text = $("#login").val();
+                    $("#btn0").click(function () {
+                        text += "0";
+                        $("#login").val(text);
+                    });
+                    $("#btn1").click(function () {
+                        text += "1";
+                        $("#login").val(text);
+                    });
+                    $("#btn2").click(function () {
+                        text += "2";
+                        $("#login").val(text);
+                    });
+                    $("#btn3").click(function () {
+                        text += "3";
+                        $("#login").val(text);
+                    });
+                    $("#btn4").click(function () {
+                        text += "4";
+                        $("#login").val(text);
+                    });
+                    $("#btn5").click(function () {
+                        text += "5";
+                        $("#login").val(text);
+                    });
+                    $("#btn6").click(function () {
+                        text += "6";
+                        $("#login").val(text);
+                    });
+                    $("#btn7").click(function () {
+                        text += "7";
+                        $("#login").val(text);
+                    });
+                    $("#btn8").click(function () {
+                        text += "8";
+                        $("#login").val(text);
+                    });
+                    $("#btn9").click(function () {
+                        text += "9";
+                        $("#login").val(text);
+                    });
+                    $("#btnReset").click(function () {
+                        text = "";
+                        $("#login").val(text);
+                    });
+                });
+            };
+
+            document.getElementById("btn0").addEventListener("click", login);
+            document.getElementById("btn1").addEventListener("click", login);
+            document.getElementById("btn2").addEventListener("click", login);
+            document.getElementById("btn3").addEventListener("click", login);
+            document.getElementById("btn4").addEventListener("click", login);
+            document.getElementById("btn5").addEventListener("click", login);
+            document.getElementById("btn6").addEventListener("click", login);
+            document.getElementById("btn7").addEventListener("click", login);
+            document.getElementById("btn8").addEventListener("click", login);
+            document.getElementById("btn9").addEventListener("click", login);
+            document.getElementById("btnReset").addEventListener("click", login);
+        </script>
     </body>
 </html>
