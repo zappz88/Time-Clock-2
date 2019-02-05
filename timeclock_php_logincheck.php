20ffb4da000c59b2602b92daa8ee39f4d76fe5fd
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-02-05 17:59:02 -0600

    Changed name and updated code
    Code now uses OOP.

---
+++ timeclock_php_logincheck.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+$_SESSION['id'] = $_POST['id'];
+$userid = $_SESSION['id'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+// Check data
+        $sql = "Select * from employees where EmployeeID = $userid";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 0) {
+            header("Location: http://localhost/TimeClock/index.php");
+        } else {
+            header("Location: http://localhost/TimeClock/HomePage.php");
+        }
+
+        $conn->close();
+?>
