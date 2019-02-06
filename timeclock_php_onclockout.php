cf46b0f6de53616ea8ff4181fa6063242bcd6379
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-02-05 18:04:02 -0600

    Updated name and added class and hours computer
    New class allows access and filtering of sent JSON data and computer allows for calculation of hours in minutes.

---
+++ timeclock_php_onclockout.php
@@ -0,0 +1,72 @@
+<?php
+
+//Initiate session and access session variables
+session_start();
+$userid = $_SESSION['id'];
+$clockInTime = $_SESSION['clockInTime'];
+$clockInDate = $_SESSION['clockInDate'];
+$_SESSION['clockOutTime'] = $_POST['clockOutTime'];
+$clockOutTime = $_SESSION['clockOutTime'];
+$_SESSION['clockOutDate'] = $_POST['clockOutDate'];
+$clockOutDate = $_SESSION['clockOutDate'];
+$clockIn = $_SESSION['clockIn'];
+
+class DataSort {
+
+    //Variables within PHP classes cannot be located outside functions at least in PHP without error
+    function DataSort() {
+        $test = json_decode($_POST['clockOutTime']);
+        $this->time = $test->time;
+        $this->hours = $test->hours;
+        $this->minutes = $test->minutes;
+    }
+
+}
+
+$clockOut = new DataSort();
+$hours = 0;
+$minutes = 0;
+
+if ($clockInTime) {
+    if ($clockOut->hours < $clockIn->hours) {
+        $hours = (12 - $clockIn->hours) + $clockOut->hours;
+        $hours = $hours * 60;
+    } else {
+        $hours = $clockOut->hours - $clockIn->hours;
+        $hours = $hours * 60;
+    }
+}
+
+if ($clockInTime) {
+    if ($clockOut->minutes < $clockIn->minutes) {
+        $minutes = -1 * ($clockOut->minutes - $clockIn->minutes);
+    } else {
+        $minutes = $clockOut->minutes - $clockIn->minutes;
+    }
+}
+
+$hours = $hours + $minutes;
+
+
+//Database connection variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Insert data
+$sql = "INSERT INTO times (UserID, Status, Date, Time, Hours) VALUES ('$userid', 'Clocked Out', '$clockOutDate', '$clockOut->time', '$hours');";
+if ($conn->query($sql) == true) {
+    echo "Data added successfully";
+    echo $hours;
+} else {
+    echo "Error adding data: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
