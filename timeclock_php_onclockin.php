6f08c7cb7aea6c93c3ed3f9da23dadf209524379
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-02-05 18:01:47 -0600

    Updated name and implemented a class
    Class used to decipher JSON code through use of constructor in order to send object through Home.php ajax call and have access to individual elements within PHP;

---
+++ timeclock_php_onclockin.php
@@ -0,0 +1,50 @@
+<?php
+
+//Initiate session and access session variables
+session_start();
+$userid = $_SESSION['id'];
+
+$_SESSION['clockInTime'] = $_POST['clockInTime'];
+$clockInTime = $_SESSION['clockInTime'];
+
+$_SESSION['clockInDate'] = $_POST['clockInDate'];
+$clockInDate = $_SESSION['clockInDate'];
+
+class DataSort {
+
+    //Variables within PHP classes cannot be located outside functions at least in PHP without error
+    function DataSort() {
+        $test = json_decode($_POST['clockInTime']);
+        $this->time = $test->time;
+        $this->hours = $test->hours;
+        $this->minutes = $test->minutes;
+    }
+
+}
+
+$clockIn = new DataSort();
+$_SESSION['clockIn'] = $clockIn;
+
+
+//Database connection variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Insert data
+$sql = "INSERT INTO times (UserID, Status, Date, Time) VALUES ('$userid', 'Clocked In', '$clockInDate', '$clockIn->time');";
+if ($conn->query($sql) == true) {
+    echo "Data added successfully";
+    echo $clockIn->time;
+} else {
+    echo "Error adding data: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
