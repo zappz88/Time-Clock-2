2d68e75b139519273cfcaa4c18857df51be9641f
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-04-21 17:24:12 -0500

    Master File Completed
    All functionality has now been moved to a singular file using a class and ajax call switch statement to return function calls for updating database. Still haven't figured out how to have the form submission respond in the same way.

--- timeClockPhpFile.php
+++ timeClockPhpFile.php
@@ -4,38 +4,26 @@ session_start();
 
 if (isset($_POST) && !empty($_POST)) {
     switch ($_POST) {
-        case 'id':
+        case filter_has_var(INPUT_POST, 'id'):
             $_SESSION['id'] = $_POST['id'];
             break;
-        case 'clockInTime':
+        case filter_has_var(INPUT_POST, 'clockInTime'):
             $_SESSION['clockInTime'] = $_POST['clockInTime'];
             break;
-        case 'clockInDate':
+        case filter_has_var(INPUT_POST, 'clockInDate'):
             $_SESSION['clockInDate'] = $_POST['clockInDate'];
             break;
-        case 'clockIn':
-            $_SESSION['clockIn'] = $_POST['clockIn'];
-            break;
-        case 'clockOutTime':
+        case filter_has_var(INPUT_POST, 'clockOutTime'):
             $_SESSION['clockOutTime'] = $_POST['clockOutTime'];
             break;
-        case 'clockOutDate':
+        case filter_has_var(INPUT_POST, 'clockOutDate'):
             $_SESSION['clockOutDate'] = $_POST['clockOutDate'];
             break;
-        case 'clockOut':
-            $_SESSION['clockOut'] = $_POST['clockOut'];
-            break;
-        case 'breakInTime':
+        case filter_has_var(INPUT_POST, 'breakInTime'):
             $_SESSION['breakInTime'] = $_POST['breakInTime'];
             break;
-        case 'breakOutTime':
+        case filter_has_var(INPUT_POST, 'breakOutTime'):
             $_SESSION['breakOutTime'] = $_POST['breakOutTime'];
-            break;
-        case 'breakTime':
-            $_SESSION['breakTime'];
-            break;
-        case 'status':
-            $_SESSION['status'] = $_POST['status'];
             break;
         default:
             print_r($_POST);
@@ -43,56 +31,48 @@ if (isset($_POST) && !empty($_POST)) {
     }
 }
 
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'mydb';
+
+if (isset($_POST['id']) && !empty($_POST['id'])) {
+
+    $userID = $_POST['id'];
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    } else {
+        $sql = "Select * from employees where EmployeeID = $userID";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 0) {
+            header("Location: http://localhost/TimeClock/index.php");
+            echo 'Error';
+        } else {
+            $_SESSION['status'] = true;
+            header("Location: http://localhost/TimeClock/homepage.php");
+        }
+
+        $conn->close();
+    }
+}
+
 class TimeClock {
 
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "mydb";
-
-    public function dataSort($data) {
-        $obj = json_decode($data);
-        $this->time = $obj->time;
-        $this->hours = $obj->hours;
-        $this->minutes = $obj->minutes;
-    }
-
-    public function checkLogin() {
-        if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            
-            $userID = $_SESSION['id'];
-
-            $conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
-
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } else {
-                $sql = "Select * from employees where EmployeeID = $userID";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows == 0) {
-                    header("Location: http://localhost/TimeClock/index.php");
-                    echo 'Error';
-                } else {
-                    $_SESSION['status'] = true;
-                    header("Location: http://localhost/TimeClock/homepage.php");
-                    echo 'Error';
-                }
-
-                $conn->close();
-            }
-        } else {
-            echo "Session userID not set.";
-        }
-    }
+    private $servername = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'mydb';
 
     public function clockIn() {
         if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            
             $userID = $_SESSION['id'];
-            $clockInTimeObj = $this->dataSort($_SESSION['clockInTime']);         
+            $clockInTimeObj = json_decode($_SESSION['clockInTime']);
             $_SESSION['clockIn'] = $clockInTimeObj;
-            $clockInDate = $_SESSION['clockInDate'];
+            $clockInDate = $_POST['clockInDate'];
 
             if ($_SESSION['status'] == true) {
 
@@ -102,10 +82,8 @@ class TimeClock {
                     die("Connection failed: " . $conn->connect_error);
                 } else {
                     $sql = "INSERT INTO times (UserID, Status, Date, Time) VALUES ('$userID', 'Clocked In', '$clockInDate', '$clockInTimeObj->hours');";
-                    if ($conn->query($sql) == true) {
+                    if ($conn->query($sql) === true) {
                         echo "Data added successfully\n";
-                        var_dump($clockInTimeObj);
-                        print_r($_SESSION);
                     } else {
                         echo "Error adding data: " . $sql . "<br>" . $conn->error;
                     }
@@ -116,32 +94,34 @@ class TimeClock {
     }
 
     public function clockOut() {
-
         if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            
             $userID = $_SESSION['id'];
-            $clockOut = $this->dataSort($SESSION['clockOutTime']);
-            $clockOutDate = $_SESSION['clockOutDate'];
+            $status = $_SESSION{'status'};
+            $clockOutTimeObj = json_decode($_SESSION['clockOutTime']);
+            $_SESSION['clockOut'] = $clockOutTimeObj;
+            $clockOutDate = $_POST['clockOutDate'];
             $clockIn = $_SESSION['clockIn'];
-            $hours = 0;            
+            $hours = 0;
             $minutes = 0;
 
-            if ($clockOut->hours < $clockIn->hours) {
-                $hours = (12 - $clockIn->hours) + ($clockOut->hours - 1);
+            if ($clockOutTimeObj->hours < $clockIn->hours) {
+                $hours = (12 - $clockIn->hours) + ($clockOutTimeObj->hours - 1);
                 $hours = $hours * 60;
             } else {
-                $hours = $clockOut->hours - $clockIn->hours;
+                $hours = $clockOutTimeObj->hours - $clockIn->hours;
                 $hours = $hours * 60;
             }
 
-            if ($clockOut->minutes < $clockIn->minutes) {
-                $minutes = $clockOut->minutes + (60 - $clockIn->minutes);
+            if ($clockOutTimeObj->minutes < $clockIn->minutes) {
+                $minutes = $clockOutTimeObj > minutes + (60 - $clockIn->minutes);
             } else {
-                $minutes = $clockOut->minutes - $clockIn->minutes;
+                $minutes = $clockOutTimeObj->minutes - $clockIn->minutes;
             }
 
             $hours = $hours + $minutes;
-            $hoursWithBreak = ($hours - $breakTime) / 60;
+            if (isset($_SESSION['breaktime']) && !empty($_SESSION['breaktime'])) {
+                $hours = ($hours - $breakTime) / 60;
+            }
 
             if ($status == true) {
                 $conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
@@ -150,7 +130,7 @@ class TimeClock {
                     die("Connection failed: " . $conn->connect_error);
                 }
 
-                $sql = "INSERT INTO times (UserID, Status, Date, Time, Hours) VALUES ('$userID', 'Clocked Out', '$clockOutDate', '$clockOut->time', '$hoursWithBreak');";
+                $sql = "INSERT INTO times (UserID, Status, Date, Time, Hours) VALUES ('$userID', 'Clocked Out', '$clockOutDate', '$clockOutTimeObj->time', '$hours');";
 
                 if ($conn->query($sql) == true) {
                     $_SESSION['status'] = false;
@@ -160,31 +140,32 @@ class TimeClock {
                 $conn->close();
             }
         }
+        print_r($_SESSION);
     }
 
     public function breakIn() {
-        $breakIn = $_SESSION['breakInTime'];
-        $breakIn = new $this->dataSort($breakInTime);
-        $breakOut = new $this->dataSort($breakOutTime);
+        $breakInTime = $_SESSION['breakInTime'];
+        $breakInTimeObj = json_decode($breakInTime);
+        $breakOutTime = $_SESSION['breakOutTime'];
+        $breakOutTimeObj = json_decode($breakOutTime);
 
         $hours = 0;
         $minutes = 0;
 
         if ($breakOutTime) {
-            if ($breakIn->hours < $breakOut->hours) {
-                $hours = (12 - $breakOut->hours) + ($breakIn->hours - 1);
+            if ($breakInTimeObj->hours < $breakOutTimeObj->hours) {
+                $hours = (12 - $breakOutTimeObj->hours) + ($breakInTimeObj->hours - 1);
                 $hours = $hours * 60;
-            } else {
-                $hours = $breakIn->hours - $breakOut->hours;
+            } 
+            else {
+                $hours = $breakInTimeObj->hours - $breakOutTimeObj->hours;
                 $hours = $hours * 60;
             }
-        }
-
-        if ($breakOutTime) {
-            if ($breakIn->minutes < $breakOut->minutes) {
-                $minutes = $breakIn->minutes + (60 - $breakOut->minutes);
-            } else {
-                $minutes = $breakIn->minutes - $breakOut->minutes;
+            if ($breakInTimeObj->minutes < $breakOutTimeObj->minutes) {
+                $minutes = $breakInTimeObj->minutes + (60 - $breakOutTimeObj->minutes);
+            } 
+            else {
+                $minutes = $breakInTimeObj->minutes - $breakOutTimeObj->minutes;
             }
         }
 
@@ -195,10 +176,11 @@ class TimeClock {
     public function breakOut() {
         $_SESSION['breakOutTime'] = $_POST['breakOutTime'];
     }
+
 }
 
-if (isset($_POST['callbackFunction']) && !empty($_POST['callbackFunction'])) {
-    switch ($_POST['callbackFunction']) {
+if (isset($_POST['functionToCall']) && !empty($_POST['functionToCall'])) {
+    switch ($_POST['functionToCall']) {
         case 'clockIn':
             $returnFunction = new TimeClock();
             $returnFunction = $returnFunction->clockIn();
