42b764ed110cd11b76b4c0521382a24312b0bb50
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-04-21 17:27:06 -0500

    Changed main php file name.

--- timeClockPhpFile.php
+++ timeClockPhpFile.php
@@ -1,206 +1,263 @@
 <?php
-
 session_start();
-
-if (isset($_POST) && !empty($_POST)) {
-    switch ($_POST) {
-        case filter_has_var(INPUT_POST, 'id'):
-            $_SESSION['id'] = $_POST['id'];
-            break;
-        case filter_has_var(INPUT_POST, 'clockInTime'):
-            $_SESSION['clockInTime'] = $_POST['clockInTime'];
-            break;
-        case filter_has_var(INPUT_POST, 'clockInDate'):
-            $_SESSION['clockInDate'] = $_POST['clockInDate'];
-            break;
-        case filter_has_var(INPUT_POST, 'clockOutTime'):
-            $_SESSION['clockOutTime'] = $_POST['clockOutTime'];
-            break;
-        case filter_has_var(INPUT_POST, 'clockOutDate'):
-            $_SESSION['clockOutDate'] = $_POST['clockOutDate'];
-            break;
-        case filter_has_var(INPUT_POST, 'breakInTime'):
-            $_SESSION['breakInTime'] = $_POST['breakInTime'];
-            break;
-        case filter_has_var(INPUT_POST, 'breakOutTime'):
-            $_SESSION['breakOutTime'] = $_POST['breakOutTime'];
-            break;
-        default:
-            print_r($_POST);
-            break;
-    }
-}
-
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'mydb';
-
-if (isset($_POST['id']) && !empty($_POST['id'])) {
-
-    $userID = $_POST['id'];
-
-    $conn = new mysqli($servername, $username, $password, $database);
-
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    } else {
-        $sql = "Select * from employees where EmployeeID = $userID";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows == 0) {
-            header("Location: http://localhost/TimeClock/index.php");
-            echo 'Error';
-        } else {
-            $_SESSION['status'] = true;
-            header("Location: http://localhost/TimeClock/homepage.php");
-        }
-
-        $conn->close();
-    }
-}
-
-class TimeClock {
-
-    private $servername = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'mydb';
-
-    public function clockIn() {
-        if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            $userID = $_SESSION['id'];
-            $clockInTimeObj = json_decode($_SESSION['clockInTime']);
-            $_SESSION['clockIn'] = $clockInTimeObj;
-            $clockInDate = $_POST['clockInDate'];
-
-            if ($_SESSION['status'] == true) {
-
-                $conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
-
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                } else {
-                    $sql = "INSERT INTO times (UserID, Status, Date, Time) VALUES ('$userID', 'Clocked In', '$clockInDate', '$clockInTimeObj->hours');";
-                    if ($conn->query($sql) === true) {
-                        echo "Data added successfully\n";
-                    } else {
-                        echo "Error adding data: " . $sql . "<br>" . $conn->error;
-                    }
-                    $conn->close();
-                }
-            }
-        }
-    }
-
-    public function clockOut() {
-        if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            $userID = $_SESSION['id'];
-            $status = $_SESSION{'status'};
-            $clockOutTimeObj = json_decode($_SESSION['clockOutTime']);
-            $_SESSION['clockOut'] = $clockOutTimeObj;
-            $clockOutDate = $_POST['clockOutDate'];
-            $clockIn = $_SESSION['clockIn'];
-            $hours = 0;
-            $minutes = 0;
-
-            if ($clockOutTimeObj->hours < $clockIn->hours) {
-                $hours = (12 - $clockIn->hours) + ($clockOutTimeObj->hours - 1);
-                $hours = $hours * 60;
-            } else {
-                $hours = $clockOutTimeObj->hours - $clockIn->hours;
-                $hours = $hours * 60;
-            }
-
-            if ($clockOutTimeObj->minutes < $clockIn->minutes) {
-                $minutes = $clockOutTimeObj > minutes + (60 - $clockIn->minutes);
-            } else {
-                $minutes = $clockOutTimeObj->minutes - $clockIn->minutes;
-            }
-
-            $hours = $hours + $minutes;
-            if (isset($_SESSION['breaktime']) && !empty($_SESSION['breaktime'])) {
-                $hours = ($hours - $breakTime) / 60;
-            }
-
-            if ($status == true) {
-                $conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
-
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-
-                $sql = "INSERT INTO times (UserID, Status, Date, Time, Hours) VALUES ('$userID', 'Clocked Out', '$clockOutDate', '$clockOutTimeObj->time', '$hours');";
-
-                if ($conn->query($sql) == true) {
-                    $_SESSION['status'] = false;
-                } else {
-                    echo "Error adding data: " . $sql . "<br>" . $conn->error;
-                }
-                $conn->close();
-            }
-        }
-        print_r($_SESSION);
-    }
-
-    public function breakIn() {
-        $breakInTime = $_SESSION['breakInTime'];
-        $breakInTimeObj = json_decode($breakInTime);
-        $breakOutTime = $_SESSION['breakOutTime'];
-        $breakOutTimeObj = json_decode($breakOutTime);
-
-        $hours = 0;
-        $minutes = 0;
-
-        if ($breakOutTime) {
-            if ($breakInTimeObj->hours < $breakOutTimeObj->hours) {
-                $hours = (12 - $breakOutTimeObj->hours) + ($breakInTimeObj->hours - 1);
-                $hours = $hours * 60;
-            } 
-            else {
-                $hours = $breakInTimeObj->hours - $breakOutTimeObj->hours;
-                $hours = $hours * 60;
-            }
-            if ($breakInTimeObj->minutes < $breakOutTimeObj->minutes) {
-                $minutes = $breakInTimeObj->minutes + (60 - $breakOutTimeObj->minutes);
-            } 
-            else {
-                $minutes = $breakInTimeObj->minutes - $breakOutTimeObj->minutes;
-            }
-        }
-
-        $hours = ($hours + $minutes) / 60;
-        $_SESSION['breakTime'] = $hours;
-    }
-
-    public function breakOut() {
-        $_SESSION['breakOutTime'] = $_POST['breakOutTime'];
-    }
-
-}
-
-if (isset($_POST['functionToCall']) && !empty($_POST['functionToCall'])) {
-    switch ($_POST['functionToCall']) {
-        case 'clockIn':
-            $returnFunction = new TimeClock();
-            $returnFunction = $returnFunction->clockIn();
-            echo 'Function Call Successful\n';
-            break;
-        case 'clockOut':
-            $returnFunction = new TimeClock();
-            $returnFunction = $returnFunction->clockOut();
-            echo 'Function Call Successful\n';
-            break;
-        case 'breakIn':
-            $returnFunction = new TimeClock();
-            $returnFunction = $returnFunction->breakIn();
-            echo 'Function Call Successful\n';
-            break;
-        case 'breakOut':
-            $returnFunction = new TimeClock();
-            $returnFunction = $returnFunction->breakOut();
-            echo 'Function Call Successful\n';
-            break;
-    }
-}
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Time Clock</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="homepagecss.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+
+        <div class="main">
+
+            <h3 id="time">Current time is: </h3>
+            <div class="btns-cont">
+                <div class="btns-left">
+                    <p id="clockin">Clock-in time: </p>
+                    <button type="button" id="clockInBtn" class="btn">Clock In</button>
+                    <p id="breakout">Break-out time: </p>
+                    <button type="button" id="breakOutBtn" class="btn">Break Out</button>
+                </div>
+
+                <div class="btns-right">
+                    <p id="clockout">Clock-out time: </p>
+                    <button type="button" id="clockOutBtn" class="btn">Clock Out</button>
+                    <p id="breakin">Break-in time: </p>
+                    <button type="button" id="breakInBtn" class="btn">Break In</button>
+                </div>
+            </div>
+            <div id="exit" class="exit"><button type='button' id="exitBtn" class="exitBtn">Exit</button></div>
+        </div>
+
+        <script>
+            var clock = {
+                bool: false,
+                in: 0,
+                inText: 'Clock-in time: ',
+                inText2: 'Already clocked in: ',
+                out: 0,
+                outText: 'Not clocked in.',
+                outText2: 'Clock-out time is: '
+            };
+
+            var rest = {
+                bool: false,
+                out: 0,
+                outText: 'Break-out time is: ',
+                outText2: 'Already on break: ',
+                in: 0,
+                inText: 'Not on break.',
+                inText2: 'Break-in time is: '
+            };
+
+            function clockIn(id) {
+                switch (clock.bool) {
+                    case false:
+                        let d = new Date().toLocaleTimeString();
+                        clock.bool = true;
+                        clock.in = d;
+                        document.getElementById(id).innerHTML = clock.inText + clock.in;
+                        onClockIn();
+                        break;
+                    case true:
+                        document.getElementById(id).innerHTML = clock.inText2 + clock.in;
+                        break;
+                }
+            };
+
+            function clockOut(id) {
+                switch (clock.bool) {
+                    case false:
+                        document.getElementById(id).innerHTML = clock.outText;
+                        break;
+                    case true:
+                        let d = new Date().toLocaleTimeString();
+                        clock.bool = false;
+                        clock.out = d;
+                        document.getElementById(id).innerHTML = clock.outText2 + clock.out;
+                        onClockOut();
+                        break;
+                }
+            };
+
+
+            function breakOut(id) {
+                if(clock.bool === true){
+                    switch (rest.bool) {
+                        case false:
+                            var d = new Date().toLocaleTimeString();
+                            rest.bool = true;
+                            rest.out = d;
+                            document.getElementById(id).innerHTML = rest.outText + rest.out;
+                            onBreakOut();
+                            break;
+                        case true:
+                            document.getElementById(id).innerHTML = rest.outText2 + rest.out;
+                            break;
+                            }
+                        }
+                else{
+                    document.getElementById(id).innerHTML = clock.outText;
+                }
+            };
+
+            function breakIn(id) {
+                if(clock.bool === true){
+                    switch (rest.bool) {
+                        case false:
+                            document.getElementById(id).innerHTML = rest.inText;
+                            break;
+                        case true:
+                            var d = new Date().toLocaleTimeString();
+                            rest.bool = false;
+                            rest.in = d;
+                            document.getElementById(id).innerHTML = rest.inText2 + rest.in;
+                            onBreakIn();
+                            break;
+                            }
+                }
+                else{
+                    document.getElementById(id).innerHTML = clock.outText;
+                    }
+            };
+
+            var myVar = setInterval(myTimer, 1000);
+            function myTimer() {
+                var d = new Date().toLocaleTimeString();
+                document.getElementById('time').innerHTML = 'Current time is: ' + d;
+            };
+
+            function resetBtnTxt(id, text) {
+                setTimeout(function () {
+                    document.getElementById(id).innerHTML = text;
+                }, 3000);
+            };
+
+            function currentDate() {
+                let date = new Date();
+                let year = date.getFullYear();
+                let month = date.getMonth() + 1;
+                let day = date.getDate();
+                return month + '/' + day + '/' + year;
+            };
+
+            function currentTime() {
+                let date = new Date();
+                let hours = date.getHours();
+                if (hours > 12) {
+                    hours = hours - 12;
+                };
+                let minutes = date.getMinutes();
+                if (minutes < 10) {
+                    minutes = '0' + minutes;
+                };
+                let time = {
+                    hours: hours,
+                    minutes: minutes,
+                    time: hours + ':' + minutes
+                };
+                return JSON.stringify(time);
+            };
+
+            function onClockIn() {
+                var xhr = new XMLHttpRequest();
+                var data = {
+                    date: 'clockInDate=' + currentDate(),
+                    time: 'clockInTime=' + currentTime(),
+                    functionToCall: 'functionToCall=clockIn'
+                };
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+                    }
+                };
+                xhr.open('POST', 'timeclockPHPMaster.php', true);
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
+            };
+
+
+            function onClockOut() {
+                var xhr = new XMLHttpRequest();
+                var data = {
+                    date: 'clockOutDate=' + currentDate(),
+                    time: 'clockOutTime=' + currentTime(),
+                    functionToCall: 'functionToCall=clockOut'
+                };
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+                    }
+                };
+                xhr.open('POST', 'timeclockPHPMaster.php', true);
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
+            };
+
+            function onBreakOut() {
+                var xhr = new XMLHttpRequest();
+                var data = {
+                    date: 'breakOutDate=' + currentDate(),
+                    time: 'breakOutTime=' + currentTime(),
+                    functionToCall: 'functionToCall=breakOut'
+                };
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+                    }
+                };
+                xhr.open('POST', 'timeclockPHPMaster.php', true);
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
+            };
+
+            function onBreakIn() {
+                var xhr = new XMLHttpRequest();
+                var data = {
+                    date: 'breakInDate=' + currentDate(),
+                    time: 'breakInTime=' + currentTime(),
+                    functionToCall: 'functionToCall=breakIn'
+                };
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.responseText);
+                    }
+                };
+                xhr.open('POST', 'timeclockPHPMaster.php', true);
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
+            };
+
+            function exit() {
+                location.href = 'http://localhost/TimeClock/index.php';
+            };
+
+            document.getElementById('exitBtn').addEventListener('click', exit);
+
+            document.getElementById('clockInBtn').addEventListener('click', function () {
+                clockIn('clockin');
+                resetBtnTxt('clockin', clock.inText);
+            });
+
+            document.getElementById('clockOutBtn').addEventListener('click', function () {
+                clockOut('clockout');
+                resetBtnTxt('clockout', clock.outText2);
+            });
+
+            document.getElementById('breakOutBtn').addEventListener('click', function () {
+                breakOut('breakout');
+                resetBtnTxt('breakout', rest.outText);
+            });
+
+            document.getElementById('breakInBtn').addEventListener('click', function () {
+                breakIn('breakin');
+                resetBtnTxt('breakin', rest.inText2);
+            });
+        </script>
+    </body>
+</html>
