a5962ce37d5c1b2088b99d61ce1c1fd46483980e
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-04-29 18:01:24 -0500

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -49,7 +49,7 @@ session_start();
                 bool: false,
                 out: 0,
                 outText: 'Break-out time is: ',
-                outText2: 'Already on break: ',
+                outText2: 'Still on break: ',
                 in: 0,
                 inText: 'Not on break.',
                 inText2: 'Break-in time is: '
@@ -68,26 +68,32 @@ session_start();
                         document.getElementById(id).innerHTML = clock.inText2 + clock.in;
                         break;
                 }
-            };
+            }
+            ;
 
             function clockOut(id) {
-                switch (clock.bool) {
-                    case false:
-                        document.getElementById(id).innerHTML = clock.outText;
-                        break;
-                    case true:
-                        let d = new Date().toLocaleTimeString();
-                        clock.bool = false;
-                        clock.out = d;
-                        document.getElementById(id).innerHTML = clock.outText2 + clock.out;
-                        onClockOut();
-                        break;
+                if (rest.bool === false) {
+                    switch (clock.bool) {
+                        case false:
+                            document.getElementById(id).innerHTML = clock.outText;
+                            break;
+                        case true:
+                            let d = new Date().toLocaleTimeString();
+                            clock.bool = false;
+                            clock.out = d;
+                            document.getElementById(id).innerHTML = clock.outText2 + clock.out;
+                            onClockOut();
+                            break;
+                    }
+                } else {
+                    document.getElementById(id).innerHTML = rest.outText2 + rest.out;
                 }
-            };
+            }
+            ;
 
 
             function breakOut(id) {
-                if(clock.bool === true){
+                if (clock.bool === true) {
                     switch (rest.bool) {
                         case false:
                             var d = new Date().toLocaleTimeString();
@@ -99,15 +105,15 @@ session_start();
                         case true:
                             document.getElementById(id).innerHTML = rest.outText2 + rest.out;
                             break;
-                            }
-                        }
-                else{
+                    }
+                } else {
                     document.getElementById(id).innerHTML = clock.outText;
                 }
-            };
+            }
+            ;
 
             function breakIn(id) {
-                if(clock.bool === true){
+                if (rest.bool === true) {
                     switch (rest.bool) {
                         case false:
                             document.getElementById(id).innerHTML = rest.inText;
@@ -119,24 +125,26 @@ session_start();
                             document.getElementById(id).innerHTML = rest.inText2 + rest.in;
                             onBreakIn();
                             break;
-                            }
-                }
-                else{
-                    document.getElementById(id).innerHTML = clock.outText;
                     }
-            };
+                } else {
+                    document.getElementById(id).innerHTML = clock.outText;
+                }
+            }
+            ;
 
             var myVar = setInterval(myTimer, 1000);
             function myTimer() {
                 var d = new Date().toLocaleTimeString();
                 document.getElementById('time').innerHTML = 'Current time is: ' + d;
-            };
+            }
+            ;
 
             function resetBtnTxt(id, text) {
                 setTimeout(function () {
                     document.getElementById(id).innerHTML = text;
                 }, 3000);
-            };
+            }
+            ;
 
             function currentDate() {
                 let date = new Date();
@@ -144,29 +152,34 @@ session_start();
                 let month = date.getMonth() + 1;
                 let day = date.getDate();
                 return month + '/' + day + '/' + year;
-            };
+            }
+            ;
 
             function currentTime() {
                 let date = new Date();
                 let hours = date.getHours();
                 if (hours > 12) {
                     hours = hours - 12;
-                };
+                }
+                ;
                 let minutes = date.getMinutes();
                 if (minutes < 10) {
                     minutes = '0' + minutes;
-                };
+                }
+                ;
                 let time = {
                     hours: hours,
                     minutes: minutes,
                     time: hours + ':' + minutes
                 };
                 return JSON.stringify(time);
-            };
+            }
+            ;
 
             function onClockIn() {
                 var xhr = new XMLHttpRequest();
                 var data = {
+                    status: 'status=true',
                     date: 'clockInDate=' + currentDate(),
                     time: 'clockInTime=' + currentTime(),
                     functionToCall: 'functionToCall=clockIn'
@@ -178,13 +191,15 @@ session_start();
                 };
                 xhr.open('POST', 'timeclockPHPMaster.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
-            };
+                xhr.send(`${data.status}&${data.date}&${data.time}&${data.functionToCall}`);
+            }
+            ;
 
 
             function onClockOut() {
                 var xhr = new XMLHttpRequest();
                 var data = {
+                    status: 'status=false',
                     date: 'clockOutDate=' + currentDate(),
                     time: 'clockOutTime=' + currentTime(),
                     functionToCall: 'functionToCall=clockOut'
@@ -196,8 +211,9 @@ session_start();
                 };
                 xhr.open('POST', 'timeclockPHPMaster.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
-            };
+                xhr.send(`${data.status}&${data.date}&${data.time}&${data.functionToCall}`);
+            }
+            ;
 
             function onBreakOut() {
                 var xhr = new XMLHttpRequest();
@@ -214,7 +230,8 @@ session_start();
                 xhr.open('POST', 'timeclockPHPMaster.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
-            };
+            }
+            ;
 
             function onBreakIn() {
                 var xhr = new XMLHttpRequest();
@@ -231,11 +248,13 @@ session_start();
                 xhr.open('POST', 'timeclockPHPMaster.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 xhr.send(`${data.date}&${data.time}&${data.functionToCall}`);
-            };
+            }
+            ;
 
             function exit() {
                 location.href = 'http://localhost/TimeClock/index.php';
-            };
+            }
+            ;
 
             document.getElementById('exitBtn').addEventListener('click', exit);
 
